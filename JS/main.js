7b47d63ab280b074a1c6eb4dd99e80ba974dadc9
adhamfayrouz <70940360+adhamfayrouz@users.noreply.github.com>
2021-06-30 22:15:58 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+    var winh = $(window).height();
+    $('#mycover').height(winh);
+    $(window).on('resize',function(){
+        $('#mycover').height($(this).height());
+    });
+
+    $(window).scroll(function(){
+        var ht = $('.cover-head').offset().top;
+        var hh = $('.cover-head').outerHeight();
+        var wh = $(window).height();
+        var ws = $(window).scrollTop();
+        var brand = $()
+        if(ws >= ht){
+
+            $('.cover-head .brand').hide();
+            $('.main-nav .brand').show();
+            $('.main-nav .nav-overlay').css('height','100%');
+        }else{
+            $('.main-nav .nav-overlay').css('height','0');
+            $('.main-nav .brand').hide();
+            $('.cover-head .brand').show();
+        }
+    });
+
+    $('.scroller').click(function(){
+
+        var target = $(this).data('target');
+        var targetH = $('' + target);
+        $('html, body').stop().animate({
+            'scrollTop': targetH.offset().top
+        }, 700, 'swing', function () {
+            window.location.hash = targetH;
+        });
+        
+    });
+
+    for(var i =0;i<47;i++){
+        $('.beats').append('<div class="bar"></div>');
+    }
+
+    $('.music-player-toggler').click(function(){
+
+        var musicBox = $(this).parent();
+        if(musicBox.css('bottom') == '30px'){
+            musicBox.css('bottom','-350px');
+        }else{
+            musicBox.css('bottom','30px');
+        }
+    });
+
+    $('.music-player iframe').on('load',function(){
+
+        $('.load-overlay .spinner').fadeOut(700,
+            function () {
+        
+                $(this).parent().fadeOut(700,
+                function () {
+        
+                    $(this).remove();
+                    
+                });
+        
+            });
+    });
+
+
+});
